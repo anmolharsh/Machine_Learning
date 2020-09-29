09fcd92d2b2c0d2a7ab3a1613786791a13ae1dae
saintlyk1d <ayudh112000@gmail.com>
2020-09-29 23:39:29 +0530

    Add attribute info file and created the attribute_set class

--- Assgn1/main.py
+++ Assgn1/main.py
@@ -1,15 +1,28 @@
 import csv
 
-class attributes_set:
-	attributes = {}
-	attributes['class'] = {'recurrence-events','recurrence-events'}
-
+class attribute_set:
+	attribute_names = []
+	attribute_values = []
+	
+# hi
 def main():
+	with open('attribute_info.txt',newline = '') as f : 	
+		content = f.read().splitlines();
+		i = 0
+		for x in content :
+			line = x.split(", ")
+			attribute_set.attribute_names.append(line[0]);
+			temp_list = []
+			for y in line[1:] :
+				temp_list.append(y);
+			attribute_set.attribute_values.append(temp_list)
+
+		print(attribute_set.attribute_names)
+		print(attribute_set.attribute_values)
 	with open('breast-cancer.csv',newline = '') as f :
 		reader = csv.reader(f);
-		for row in reader :
-			print(', '.join(row))
-	print(attributes_set.attributes)
+
+	
 
 if __name__ == '__main__':
     main()
