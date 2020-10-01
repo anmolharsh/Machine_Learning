ffcfc1ad3cc807bc6b8c4f9afaf2839efa1041b8
anmolharsh <anmolharsh@iitkgp.ac.in>
2020-10-01 13:46:21 +0530

    id3 started/ base condtn

--- Assgn1/main.py
+++ Assgn1/main.py
@@ -4,6 +4,11 @@ import sys
 import math
 import os
 
+
+positive="recurrence-events"
+negative="no-recurrence-events"
+
+
 class attribute_set:
 	attribute_names = []
 	attribute_values = []
@@ -30,13 +35,49 @@ class node :
 
 # class tree :
 
-# def calc_info_gain(attribute_number,example_list) :
+def calc_info_gain(attribute_number,example_list) :
 
 
-# def build_tree(max_depth,data_set) :
-# 	for i in 
+def pos(example_list):
+	for x in example_list:
+		if x[0]==negative:
+			return 0
+	return 1
+
+def neg(example_list):
+	for x in example_list:
+		if x[0]==positive:
+			return 0
+	return 1
 
 
+def base_check(max_depth,example_list,all_att,vis):
+	if max_depth==0:
+		return 1
+	else:
+		if pos(example_list):
+			return 1
+		elif neg(example_list):
+			return 1
+		for x in vis:
+			if x==0:
+				return 0
+		return 1
+	return 0
+
+
+def build_tree(max_depth,example_list,target, all_att,vis) :
+	if base_check(max_depth,example_list,all_att,vis)==1 :
+		#make leaf
+		return 1 # return node
+	else:
+		max_att=0
+		index=0
+		for x in vis:
+			if x==0:
+				max_att=max(max_att,calc_info_gain(index,example_list))
+			index+=1
+		return 0
 
 
 def main():
@@ -58,12 +99,17 @@ def main():
 	with open('breast-cancer.csv',newline = '') as f :
 		reader = csv.reader(f)
 		example_list = list(reader)
+		# print(example_list)
 
 
 	d1 = data_set(len(example_list),example_list)	
 	# print(d1.example_list,d1.sz)
 	new_node = node(1)
 	print(new_node.child)
+	vis=[]
+	for x in range(len(attribute_set.attribute_names)):
+		vis.append(0)
+	vis[0]=1
 
 
 if __name__ == '__main__':
