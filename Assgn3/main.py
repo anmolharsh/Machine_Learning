c12e246d860f775a52c50f24bd2421ad8c40e888
saintlyk1d <ayudh112000@gmail.com>
2020-11-13 18:41:06 +0530

    Part2 3/4 completed

--- Assgn3/main.py
+++ Assgn3/main.py
@@ -8,10 +8,13 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import time
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
+from sklearn.utils.testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
 
 class attribute_set :
 
@@ -35,7 +38,81 @@ def handle_missing_values(attribute_set,example_list) :
 					example_list.at[i,x] = means[x]
 				else :
 					example_list.at[i,x] = modes.at[0,x]
+def calc_accuracy(Y_test, Y_predicted) :
+	sz = len(Y_test)
+	count = 0
+	for x,y in zip(Y_test, Y_predicted) :
+		count += (x == y)
 
+	return count/sz
+
+@ignore_warnings(category=ConvergenceWarning)
+def mlp_classification(attribute_set, training_set, test_set) :
+	X_train = training_set.iloc[:,0:-1].values
+	Y_train = training_set.iloc[:,-1].values
+
+	X_test = test_set.iloc[:,0:-1].values
+	Y_test = test_set.iloc[:,-1].values
+
+	
+
+	architectures = [(), (2), (6), (2,3), (3,2)]
+
+	# comparing accuracy vs learning rate for each model
+	for i,hidden_layer in zip(range(5),architectures) :
+		rate = 0.1
+		x_pts = []
+		y_pts = []
+		for x in range(5) :
+			x_pts.append(rate)
+			classifier = MLPClassifier(solver = 'sgd',hidden_layer_sizes = hidden_layer, learning_rate_init = rate)
+			classifier.fit(X_train,Y_train)
+			Y_predicted = classifier.predict(X_test)
+			y_pts.append(calc_accuracy(Y_test, Y_predicted))
+			rate /= 10
+
+		# line graph
+		fig = plt.figure()
+		plt.xlabel('Learning Rate')  # Add an x-label to the axes.
+		plt.ylabel('Accuracy')  # Add a y-label to the axes.
+		plt.title("Accuracy vs. Learning Rate")  # Add a title to the axes.
+		plt.semilogx(x_pts, y_pts)
+		file_name = "graph_architecture_" + str(i)
+		plt.savefig(file_name)
+
+	# comapring accuracy vs architecture for each learning rate
+	rate = 0.1
+	for i in range(5) :
+		x_pts = []
+		y_pts = []
+		exec_time = []
+		for j,x in zip(range(5),architectures) :
+			start_time = time.time()
+			classifier = MLPClassifier(activation = "logistic", solver = 'sgd',hidden_layer_sizes = x, learning_rate_init = rate)
+			classifier.fit(X_train,Y_train)
+			exec_time.append(time.time()-start_time)
+			Y_predicted = classifier.predict(X_test)
+			x_pts.append("Model " + str(j))
+			y_pts.append(calc_accuracy(Y_test, Y_predicted))
+		print(exec_time,"\n")
+		# bar graph
+		fig = plt.figure()
+		plt.xlabel('Model')  # Add an x-label to the axes.
+		plt.ylabel('Accuracy')  # Add a y-label to the axes.
+		plt.title("Accuracy vs Model for Learning Rate = " + str(rate))  # Add a title to the axes.
+		plt.ylim([0,1])
+		plt.bar(x = np.array(x_pts), height = np.array(y_pts), width = 0.4)
+		file_name = "graph_learning_rate_" + str(i) + ".png"
+		plt.savefig(file_name)
+
+		rate /= 10
+	
+
+
+
+
+	
+	
 
 def main() :
 
@@ -88,22 +165,9 @@ def main() :
 	print(training_set)
 	print(test_set)
 
-	X_train = training_set.iloc[:,0:-1].values
-	Y_train = training_set.iloc[:,-1].values
+	mlp_classification(attribute_set, training_set, test_set)
 
-	X_test = test_set.iloc[:,0:-1].values
-	Y_test = test_set.iloc[:,-1].values
-
-	print(X_train)
-	print(Y_train)
-
-	print(X_test)
-	print(Y_test)
-
-	classifier = MLPClassifier(solver = 'sgd',hidden_layer_sizes = ())
-
-	classifier.fit(X_train,Y_train)
-	print(classifier.predict(X_test))
+	
 	
 
 
